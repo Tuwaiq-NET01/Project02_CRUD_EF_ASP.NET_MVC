84db486465dc9e809ab2a618cd202124c1766f6e
Mohammed Rashed <Mohammed.Albalawi@tuwaiq.edu.sa>
2021-06-11 15:13:41 +0300

    front end work (Navbar, home, footer)

--- GameInfo/GameInfo/ClientApp/src/App.js
+++ GameInfo/GameInfo/ClientApp/src/App.js
@@ -1,23 +1,99 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import React, { useState, useEffect } from "react";
 import './custom.css'
-import Games from './components/Games';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Footer from "./components/Footer";
 
-export default class App extends Component {
-  static displayName = App.name;
+// import "./App.css";
+import HomePage from "./components/HomePage";
+import { MdGames } from "react-icons/md";
+import { BsFillHeartFill } from "react-icons/bs";
+import { AiFillHome } from "react-icons/ai";
+import { AiFillInfoCircle } from "react-icons/ai";
+import About from "./components/About";
+import Games from "./components/Games";
 
-  render () {
-    return (
-        <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/games' component={Games} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
-    );
-  }
+export default function App() {
+  return (
+    <>
+      <Router>
+        <div>
+          <nav
+            class="navbar navbar-expand-lg navbar-dark bg-primary"
+            style={{ fontFamily: "Mate SC, serif", fontSize: "22px" }}
+          >
+            <div class="container-fluid">
+              <a class="navbar-brand" href="/">
+                GameList
+              </a>
+              <div class="collapse navbar-collapse" id="navbarColor01">
+                <ul class="navbar-nav me-auto">
+                  <li class="nav-item mx-4">
+                    <Link className="nav-link" to="/">
+                      <AiFillHome size={30} /> Home
+                    </Link>
+                  </li>
+                  <li class="nav-item mx-4">
+                    <Link className="nav-link" to="/games">
+                      <MdGames size={30} /> Games
+                    </Link>
+                  </li>
+                  {/* <li class="nav-item mx-4">
+                    <Link className="nav-link" to="/favorites">
+                      <BsFillHeartFill size={30} /> Favorites
+                    </Link>
+                  </li> */}
+                  <li class="nav-item mx-4">
+                    <Link className="nav-link" to="/about">
+                      <AiFillInfoCircle size={30} /> About
+                    </Link>
+                  </li>
+                </ul>
+
+                <form class="d-flex">
+                  {/* <input
+                    class="form-control me-sm-2 m-2"
+                    type="text"
+                    placeholder="Search"
+                    onChange={(e) => setSearchInput(e.target.value)}
+                  />
+
+                  <Link className="nav-link" to="/search">
+                    <button
+                      class="btn btn-secondary my-2 my-sm-0"
+                      type="submit"
+                    >
+                      Search
+                    </button>
+                  </Link>
+                 */}
+                </form>
+              </div>
+            </div>
+          </nav>
+
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={() => <HomePage />}
+            />
+
+            <Route
+              exact
+              path="/games"
+              render={() => <Games />}
+            />
+
+
+            <Route exact path="/about" render={() => <About />} />
+
+
+          </Switch>
+        </div>
+      </Router>
+
+      <Footer />
+    </>
+  )
 }
+
--- GameInfo/GameInfo/ClientApp/src/components/Games.jsx
+++ GameInfo/GameInfo/ClientApp/src/components/Games.jsx
@@ -1,11 +1,13 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
+import {BsFillStarFill} from 'react-icons/bs';
+
 
 export default function Games() {
     const [allGames, setAllGames] = useState([])
     const [gameName, setGameName] = useState('');
     const [gameDesc, setGameDesc] = useState('');
-    const [rating, setRating] = useState(0)
+    const [rating, setRating] = useState(1)
     const url = 'https://localhost:44384/api/games'
 
 
@@ -61,7 +63,8 @@ const addGame  = (e) => {
 
 
     return (
-        <div>
+        <div className="container">
+
             <h1>GAMES COMP</h1>
 
             <form>
@@ -75,23 +78,80 @@ const addGame  = (e) => {
   <label for="gameDesc" class="form-label">Example textarea</label>
   <textarea class="form-control" id="gameDesc" rows="3" onChange={(e) => setGameDesc(e.target.value)}></textarea>
 </div>
-
-<select class="form-select" aria-label="Default select example" onChange={(e) => setRating(e.target.value)}>
+{/* ={{ color: 'blue', size: '50px' }} */}
+{/* <select class="form-select" aria-label="Default select example" onChange={(e) => setRating(e.target.value)}>
   <option selected>Rating</option>
-  <option value="1">One</option>
-  <option value="2">Two</option>
-  <option value="3">Three</option>
-</select>
+  <option value="1"> aaaaaa <BsFillStarFill /> </option>
+  <option value="2"><BsFillStarFill /> <BsFillStarFill /></option>
+  <option value="3"><BsFillStarFill /> <BsFillStarFill /> <BsFillStarFill /></option>
+</select> */}
+
+<div onChange={(e)=> setRating(e.target.value)}> 
+<div class="form-check">
+  <input class="form-check-input" type="radio" name="ratingRadio" value="1" id="flexRadioDefault1" checked/>
+  <label class="form-check-label" for="flexRadioDefault1">
+  <BsFillStarFill />
+  </label>
+</div>
+<div class="form-check">
+  <input class="form-check-input" type="radio" name="ratingRadio" value="2"  id="flexRadioDefault2"  />
+  <label class="form-check-label" for="flexRadioDefault2">
+    <BsFillStarFill /> <BsFillStarFill />
+  </label>
+</div>
+<div class="form-check">
+  <input class="form-check-input" type="radio" name="ratingRadio" value="3"  id="flexRadioDefault3"  />
+  <label class="form-check-label" for="flexRadioDefault3">
+    <BsFillStarFill /> <BsFillStarFill /> <BsFillStarFill />
+  </label>
+</div>
+<div class="form-check">
+  <input class="form-check-input" type="radio" name="ratingRadio" value="4"  id="flexRadioDefault4"  />
+  <label class="form-check-label" for="flexRadioDefault4">
+    <BsFillStarFill /> <BsFillStarFill /> <BsFillStarFill /> <BsFillStarFill />
+  </label>
+</div>
+<div class="form-check">
+  <input class="form-check-input" type="radio" name="ratingRadio" value="5"  id="flexRadioDefault5"  />
+  <label class="form-check-label" for="flexRadioDefault5">
+    <BsFillStarFill /> <BsFillStarFill /> <BsFillStarFill /> <BsFillStarFill /> <BsFillStarFill />
+  </label>
+</div>
+</div>
 
 </form>
-  <button  class="btn btn-primary" onClick={(e) => addGame(e)}>ADD GAME</button>
+  <button  class="btn btn-primary" onClick={(e) => addGame(e)}>ADD GAME  </button>
+
+  <i class="fas fa-star fa-2x"></i>
 
 
-{allGames.map((g) => {
-    return(
-        <h3>nnn : {g.gameName} <button onClick={() => deleteGame(g.id)}> DELETE </button></h3>
+
+<div className="row">{allGames.map((ga, i) => {
+    return (
+              <div className="col-md-3 mt-5" key={i}>
+        <div className="card text-white bg-dark" style={{ width: "18rem" }}>
+          <img
+            src=""
+            className="card-img-top"
+            alt="game picture"
+            width="200px"
+            height="250px"
+          />
+          <div className="card-body">
+            <h5 className="card-title"> {ga.gameName} </h5>
+            <p className="card-text">
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, quam.
+            </p>
+          </div>
+          <button className="btn btn-danger" onClick={() => deleteGame(ga.id)}> DELETE </button>
+        </div>
+      </div>
+        
     )
-})}
+})}</div>
+
+
+
 
         </div>
     )
