0f7ebc546d548a6025af5ffd3bd5776ac18ac1d9
Hanan <82478654+hanan-tuwaiq@users.noreply.github.com>
2021-06-12 19:52:56 +0300

    Edit: final styles

---
+++ blog/WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Data;
+
+namespace WebApplication1.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly AppDbContext _db;
+        public UserController(AppDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            ViewData["users"] = _db.Users.ToList();
+            return View();
+        }
+    }
+}
